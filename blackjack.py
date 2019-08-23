e6407ac7913a46f2118e318935c532ba1fef6024
AdamOlsson <Olsson.95_@hotmail.com>
2019-08-23 16:26:37 +0200

    Kinda workds

--- blackjack.py
+++ blackjack.py
@@ -8,21 +8,23 @@ class BlackJack():
         self.actions = {0:'Stick', 1:'Hit'}
         self.nS = 19 # Sum of hand from 4 to 21, a hand > 22 is terminal state
 
+        self.policy_shape = (19,10,2,self.nA)
+        self.value_shape = (19,10,2)
+
     def deal_card(self):
         return random.randint(2,11)
 
     def gen_episode(self, policy):
 
         episode = []
-
         player = Hand(hand=[random.randint(2,11), random.randint(2,11)])
         dealer = Hand(hand=[random.randint(2,11), random.randint(2,11)])
-
+        
         # Players turn
         while True:
 
             state = (player.sum, dealer.hand[0], player.has_usable_ace)
-            action = policy[state[0]][state[1]][state[2]]
+            action = np.argmax(policy[max(22, state[0])-4][state[1]-2][1 if state[2] else 0])
 
             # Bust
             if player.bust:
@@ -40,17 +42,18 @@ class BlackJack():
         while True:
             # Bust
             if dealer.bust:
-                episode.append((state, 0, 1))
+                episode.append((state, action, 1))
                 return episode
             # Dealer win
             elif dealer.sum == 21:
-                episode.append((state, 0, -1 if not player.sum == 21 else 0))
+                episode.append((state, action, -1 if not player.sum == 21 else 0))
                 return episode
             else:
                 if dealer.sum < 17:
                     dealer.add_card(self.deal_card())
                 else:
-                    return episode.append((state, dealer.hand[0], max(-1, min(1, player.sum-dealer.sum))))
+                    episode.append((state, action, max(-1, min(1, player.sum-dealer.sum))))
+                    return episode
 
             
 
@@ -58,5 +61,8 @@ class BlackJack():
 if __name__ == "__main__":
     env = BlackJack()
 
-    policy = np.ones([env.nS, env.nA]) / env.nA
-    env.gen_episode([])
+    policy = np.ones(env.policy_shape) / env.nA
+
+    for i in range(1000):
+        #env.gen_episode(policy)
+        print(env.gen_episode(policy))
